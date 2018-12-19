fc3ed061457972f239a05c11c5817050d0f2b24c
militho <militho.blazitho@gmail.com>
2018-12-19 14:17:45 -0600

    carga de productos desde firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,16 +10,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo : any[] = [];
 
   constructor( private http: HttpClient ) {
-    console.log('servicio listo')
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
              .subscribe( (resp: InfoPagina) => {
                this.cargada = true;
                this.info = resp;
-               console.log(resp)
              });
   }
+
+  private cargarEquipo(){
+    // Leer archivo JSON
+    this.http.get('https://cms-yapsi.firebaseio.com/equipo.json')
+             .subscribe( (resp: any[]) => {
+               this.equipo = resp
+             });
+  }
+
 }
